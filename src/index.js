62801476ede52490b61650de98bce88752bc2095
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-04-12 07:56:27 +0800

    setup awal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./css/style.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <p className='font-bold text-4xl'>Hello world this is home</p>
+    </>
+  );
+}
